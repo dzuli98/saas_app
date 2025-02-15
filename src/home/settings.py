19f4aeccc42fc13135bef71d7ccfd1c13da955e1
Julija Filipovic <filipovicjulija98@gmail.com>
2025-02-16 00:08:17 +0100

    prepare for railway

--- src/home/settings.py
+++ src/home/settings.py
@@ -25,8 +25,14 @@ SECRET_KEY = 'django-insecure-f*b388b_1$6lkoj@)#j4tjlqbz3@(&+3c&qtgt%%x^m3=fnm=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app/
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "localhost",
+        "127.0.0.1"
+        ]
 
 # Application definition
 
