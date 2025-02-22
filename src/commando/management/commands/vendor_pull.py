58ac48345f55a4b45bea6755c6a2270e6826eb1c
Julija Filipovic <filipovicjulija98@gmail.com>
2025-02-22 23:31:42 +0100

    add whitenoise

--- src/commando/management/commands/vendor_pull.py
+++ src/commando/management/commands/vendor_pull.py
@@ -4,7 +4,8 @@ from django.core.management.base import BaseCommand
 
 VENDOR_STATICFILES = {
     "flowbite.min.css": "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css",
-    "flowbite.min.js": "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"
+    "flowbite.min.js": "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js",
+    "flowbite.min.js.map": "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js.map"
 }
 STATICFILES_VENDOR_DIR = getattr(settings, "STATICFILES_VENDOR_DIR")
 class Command(BaseCommand):
