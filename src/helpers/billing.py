9be3b4efb9717a952b9408c8d95aa90651e00ad3
Julija Filipovic <filipovicjulija98@gmail.com>
2025-06-06 01:09:42 +0200

    add stripe override

--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -6,8 +6,9 @@ from helpers import date_utils
 
 DJANGO_DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY', default='', cast=str)
+STRIPE_TEST_OVERRIDE = config('STRIPE_TEST_OVERRIDE', default=False, cast=bool)
 
-if 'sk_test' in STRIPE_SECRET_KEY and not DJANGO_DEBUG:
+if 'sk_test' in STRIPE_SECRET_KEY and not DJANGO_DEBUG and not STRIPE_TEST_OVERRIDE:
     raise ValueError("You are using a test secret key in production.")
 
 stripe.api_key = STRIPE_SECRET_KEY
